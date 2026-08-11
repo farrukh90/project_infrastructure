f0d71401cc2b4ab64691f355cede7596bec10e67
farrukh90 <farrukhsadykov@gmail.com>
2026-08-11 23:40:59 +0000

    added more files

---
+++ 2.tools_setup/domain.tf
@@ -0,0 +1,8 @@
+resource "google_dns_managed_zone" "project" {
+  name        = "project"
+  dns_name    = var.dns_name
+  description = "Used for project"
+  labels = {
+    managed_by = "terraform"
+  }
+}
---
+++ 2.tools_setup/variables.tf
@@ -0,0 +1,5 @@
+variable "dns_name" {
+  description = "Please specify your domain"
+  type        = string
+  default     = "example.com"
+}
