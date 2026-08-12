1b892507f297ff92fe1c57621495e8b3ef008626
farrukh90 <farrukhsadykov@gmail.com>
2026-08-12 02:27:48 +0000

    updated main.tf

--- 2.tools_setup/ingress.tf
+++ 2.tools_setup/ingress.tf
@@ -1,8 +1,21 @@
-module "name" {
+module "ingress-ns" {
+  source = "farrukh90/ns/kubernetes"
+  count  = var.ingress ? 1 : 0
+  name   = "ingress"
+  annotations = {
+    application = "ingress"
+  }
+  labels = {
+    managedby = "terraform"
+  }
+}
+
+
+module "ingress" {
   source     = "farrukh90/appdeploy/helm"
   count      = var.ingress ? 1 : 0
   name       = "nginx-ingress-controller"
-  namespace  = "default"
+  namespace  = module.ingress-ns[0].name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx-ingress-controller"
   wait       = false
