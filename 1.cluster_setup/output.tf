c0b7e19165616c1b52e6ba0337ed9341f2757983
farrukh90 <farrukhsadykov@gmail.com>
2026-08-13 18:21:45 -0500

    added proper output

--- 1.cluster_setup/output.tf
+++ 1.cluster_setup/output.tf
@@ -1,9 +1,9 @@
 output "cluster_location" {
   description = "GKE cluster location"
-  value       = google_container_cluster.primary.location
+  value       = module.demo.cluster_location
 }
 
 output "cluster_name" {
   description = "GKE cluster name"
-  value       = google_container_cluster.primary.name
+  value       = module.demo.cluster_name
 }
