7482bab0040cdce7ac5e8ebe9197b54e5c36aabb
farrukh90 <farrukhsadykov@gmail.com>
2026-08-14 15:15:25 +0000

    added prome ssl

--- 2.tools_setup/prometheus.tf
+++ 2.tools_setup/prometheus.tf
@@ -2,33 +2,41 @@ module "prometheus-ns" {
   source = "farrukh90/ns/kubernetes"
   count  = var.prometheus ? 1 : 0
   name   = "prometheus"
+
   annotations = {
     application = "prometheus"
   }
+
   labels = {
     managedby = "terraform"
   }
 }
 
-# Deploy prometheus using Helm into the prometheus namespace
 module "prometheus-terraform-helm" {
   source     = "farrukh90/appdeploy/helm"
   name       = "prometheus"
   namespace  = module.prometheus-ns[0].name
   chart      = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
+
   values = [<<EOF
-
-
 server:
   ingress:
     enabled: true
-    ingressClassName: "nginx"
+    ingressClassName: nginx
+
     annotations:
       kubernetes.io/ingress.class: nginx
-      kubernetes.io/tls-acme: 'true'
-    hosts: 
+      cert-manager.io/cluster-issuer: letsencrypt-prod
+      acme.cert-manager.io/http01-edit-in-place: "true"
+
+    hosts:
       - prometheus.${var.dns_name}
+
+    tls:
+      - secretName: prometheus-tls
+        hosts:
+          - prometheus.${var.dns_name}
 EOF
   ]
 }
