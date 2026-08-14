ec55c55fd9d16538cfbb62d8b926971bd4ed88d4
farrukh90 <farrukhsadykov@gmail.com>
2026-08-14 02:18:24 +0000

    updated provider

--- 2.tools_setup/provider.tf
+++ 2.tools_setup/provider.tf
@@ -1,21 +1,9 @@
-data "google_client_config" "default" {}
-
-data "google_container_cluster" "my_cluster" {
-  name     = var.gke_config["cluster_name"]
-  location = var.gke_config["location"]
-}
-
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  config_path = pathexpand("~/.kube/config")
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+    config_path = pathexpand("~/.kube/config")
   }
 }
-
