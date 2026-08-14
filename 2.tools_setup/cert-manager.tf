c194eb47cf962da06e062967ef306bfa688ba546
farrukh90 <farrukhsadykov@gmail.com>
2026-08-14 09:56:51 -0500

    added lets

--- 2.tools_setup/cert-manager.tf
+++ 2.tools_setup/cert-manager.tf
@@ -47,3 +47,39 @@ project_id: "${var.project_id}"
 google_domain_name: "${var.dns_name}"
 EOF
 }
+
+resource "kubernetes_manifest" "letsencrypt_prod" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+
+    metadata = {
+      name = "letsencrypt-prod"
+    }
+
+    spec = {
+      acme = {
+        email  = var.email
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+
+        privateKeySecretRef = {
+          name = "letsencrypt-prod"
+        }
+
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                ingressClassName = "nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
